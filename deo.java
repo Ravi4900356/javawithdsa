9425871bdb474927a5cfa4e97bdbd43e83f0cfb3
Ravi <ravikumardeo824202@gmail.com>
2024-10-06 12:09:12 +0530

    add

---
+++ deo.java
@@ -0,0 +1,5 @@
+public class deo{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ hello.java
@@ -0,0 +1,9 @@
+package javawithdsa;
+//
+
+public class hello{
+
+    public static void main(String[] args) {
+        System.out.println("Hi every one");
+    }
+}
